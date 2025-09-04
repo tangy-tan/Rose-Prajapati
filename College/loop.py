9cd993797a41fb8c576b0dfd74bb76d941bc5acc
Rose Prajapati <prajapati.rose1@gmail.com>
2025-09-04 17:05:24 +0545

    Removed unnecessary files

--- College/loop.py
+++ College/loop.py
@@ -1,5 +1,40 @@
+#while loop
 i=0
 while i<5:
     print(i)
     i+=1 #i=i+1,i++
     
+#infinite loop
+#-while True:
+#-  print("")
+
+name=""
+while name!="Rose":
+    name= input("Enter your name: ")
+
+#foor loop : 
+string1 = "Happy Life"
+for char in string1:
+    print(char)
+
+#range():create iterable
+print(list(range(10)))
+
+for i in range(10,2,-1):
+    print(i)
+
+#break=loop stops 
+
+while True:
+    name = input("Enter your name: ")
+    if name=="rose" :
+        break
+
+#continue= skip the loop
+for i in range(0,10):
+    if i%2==0: #even
+        continue
+    else:
+        print(i)
+
+
